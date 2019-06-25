ea2793f5d15849b179a03482b35f22bf19eeb8dc
LukeHxH <lucasmed812@gmail.com>
2019-06-25 13:32:16 -0300

    Modifica arquivo de hello world de riscv

--- oac/LISTA_EXERCICIOS_06/hello_world_riscv.s
+++ oac/LISTA_EXERCICIOS_06/hello_world_riscv.s
@@ -1,8 +1,8 @@
 .section .text
 .globl _start
 _start:
-	    lui a0,       %hi(msg)       # load msg(hi)
-	    addi a0, a0,  %lo(msg)       # load msg(lo)
+	    lui a0,       %hi(msg)
+	    addi a0, a0,  %lo(msg)
 	    jal ra, puts
 2:	    j 2b
 
